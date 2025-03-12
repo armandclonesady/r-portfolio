58c97554b887e7d146623409768d3ecf92388afd
Armand Sady <armand.sady.etu@univ-lille.fr>
2025-03-12 16:17:40 +0100

    Standard text for About page

--- portfolio/src/features/About.tsx
+++ portfolio/src/features/About.tsx
@@ -1,7 +1,23 @@
+import { Container } from "react-bootstrap";
+
 export function About() {
     return (
-        <div>
-            <h1>About</h1>
-        </div>
+        <Container className="mt-5">
+            <h1>À propos de moi</h1>
+            <br /> <br />
+            <p>
+                Je m'appelle Armand Sady, je suis actuellement en <strong>3eme année de BUT Informatique a l'Université de Lille</strong>
+            </p>
+            <p>
+            <strong>Passioné d'informatique</strong> et des jeux videos depuis l'enfance, je me suis enfin orienté vers l'informatique au lycée avec l'option SI en seconde, et plus profondément avec l'option NSI et ensuite la formation du BUT Informatique de Lille.
+            Ayant choisi le parcours <strong>Réalisation d'Application</strong>, ma formation est accès sur le développement pur, en utilisant principalement le Java, tout en nous apprennant différents autres languages.
+            Je suis actuellement <strong>à la recherche d'un stage pour de 10 semaines minimum pour Mars 2025</strong>.
+            J'aimerais continuer ma formation après le BUT afin <strong>d'obtenir un Master</strong>.
+            </p>
+            <p>
+                J'ai eu l'occasion de réalisé beaucoup de projet au cours de ma formation, et j'ai acquis aussi des compétences en informatiques qui sont visible sur ce portfolio.
+                Vous pouvez scroll afin de changer de page et voir mes compétences, mes expériences, mes formations, mes projets et mes contacts.
+            </p>
+        </Container>
     )
 }
